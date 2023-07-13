a5d7924c3190f92fff4eba7f49e5b208e31ef6ef
Mean <moenmean@gmail.coom>
2023-07-13 12:49:10 +0700

    Update PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<!--HEADER-->
+<?php
+	include 'header/header.php';
+?>
+<!--end HEADER-->
+<!--HOMEPAGE-->
+	<?php
+		include_once 'homepage.php';
+	?>
+<!--END-HOMEPAGE-->
+
+<!--FOOTER-->
+<?php
+include_once 'footer/footer.php';
+?>
+<!--END-FOOTER-->
