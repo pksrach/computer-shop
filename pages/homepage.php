243321e9d9765ed11eee46eea9cea60b0d5ea587
suonpisey01778 <pisey.suon@cubetiqs.com>
2023-10-04 19:26:28 +0700

    get in product

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,4 +1,3 @@
-
 <!-- SECTION -->
 <div class="section">
     <!-- container -->
@@ -71,7 +70,7 @@
                             $result = mysqli_query($conn, $sql);
                             while ($row = mysqli_fetch_array($result)) {
                             ?>
-                              <li ><a data-toggle="tab" href="#tab1"><?= $row[0] ?></a></li>
+                                <li><a data-toggle="tab" href="#tab1"><?= $row[0] ?></a></li>
                             <?php } ?>
                             <!-- <li class="active"><a data-toggle="tab" href="#tab1">Laptops</a></li> -->
                             <!-- <li><a data-toggle="tab" href="#tab1">Smartphones</a></li>
@@ -91,23 +90,62 @@
                         <div id="tab1" class="tab-pane active">
                             <div class="products-slick" data-nav="#slick-nav-1">
                                 <!-- product -->
-                                <?php 
-                                $sql="SELECT
+                                <?php
+                                $sql = "SELECT
                                 p.id,
                                 product__name,
                                 p.description,
                                 price,
                                 c.category_name,
                                 b.brand_name,
-                                u.unit_name,p.attatchment_url,p.`status`
+                                u.unit_name,
+                                p.attatchment_url,
+                                p.`status` 
                             FROM
                                 tbl_product p
                                 INNER JOIN tbl_category c ON p.category_id = c.id
-                                INNER JOIN tbl_brand b ON p.brand_id = b.id";
-                                  $results = mysqli_query($conn, $sql);
-                                  while ($row = mysqli_fetch_array($result)) {
+                                INNER JOIN tbl_brand b ON p.brand_id = b.id
+                                INNER JOIN tbl_unit_measurement u ON p.unit_id=u.id;";
+                                $result = mysqli_query($conn, $sql);
+                                if ($result) {
+
+
+                                    while ($row = mysqli_fetch_array($result)) {
                                 ?>
-                                <div class="product ">
+                                        <div class="product ">
+                                            <div class="product-img">
+                                                <img src="./img/product01.png" alt="">
+                                                <div class="product-label">
+                                                    <span class="sale">-30%</span>
+                                                    <span class="new">NEW</span>
+                                                </div>
+                                            </div>
+                                            <div class="product-body ">
+                                                <p class="product-category"><?= $row[4] ?></p>
+                                                <h3 class="product-name"><a href="#"><?= $row[1] ?></a></h3>
+                                                <h4 class="product-price"><?= $row[3] ?> <del class="product-old-price">$990.00</del></h4>
+                                                <div class="product-rating">
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                </div>
+                                                <div class="product-btns">
+                                                    <button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+                                                    <button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
+                                                    <button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+                                                </div>
+                                            </div>
+                                            <div class="add-to-cart">
+                                                <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+                                            </div>
+                                        </div>
+                                <?php }
+                                } else {
+                                    echo "Query failed: " . $conn->error;
+                                } ?>
+                                <!-- <div class="product ">
                                     <div class="product-img">
                                         <img src="./img/product01.png" alt="">
                                         <div class="product-label">
@@ -116,7 +154,7 @@
                                         </div>
                                     </div>
                                     <div class="product-body ">
-                                        <p class="product-category"><?php [4] ?></p>
+                                        <p class="product-category"></p>
                                         <h3 class="product-name"><a href="#">product name goes here</a></h3>
                                         <h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
                                         <div class="product-rating">
@@ -136,8 +174,7 @@
                                         <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
                                     </div>
                                 </div>
-                                <?php } ?>
-                                
+                                 -->
                                 <!-- /product -->
 
                                 <!-- product -->
@@ -172,7 +209,7 @@
                                 <!-- /product -->
 
                                 <!-- product -->
-                                    <!-- <div class="product">
+                                <!-- <div class="product">
                                         <div class="product-img">
                                             <img src="./img/product03.png" alt="">
                                             <div class="product-label">
